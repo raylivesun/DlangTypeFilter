2e44d61ae0fecf58dcc32be3b8c71229d85d176f
raylivesun <162396999+raylivesun@users.noreply.github.com>
2024-03-11 18:42:21 -0300

    DlangTypeFilter
    Dlang Type Filter Ides Reference Manual

---
+++ matrix/gnu/bin/c++/filter.c++
@@ -0,0 +1,11 @@
+
+/** 
+call of arguments to pattern logical connect the matrix
+to side vision logical template views method stables sort
+to reference utils method easy board token series level 
+linked type of references to utils typages.
+**/ 
+class MyMatrixGNUBinDlangFaps
+{
+    void faps(long hackers);
+};
---
+++ matrix/gnu/bin/dlang/like.d
@@ -0,0 +1,26 @@
+module matrix.gnu.bin.dlang.like;
+
+import std.algorithm.comparison : equal;
+
+/** 
+call of arguments to pattern logical connect the matrix
+to side vision logical template views method stables sort
+to reference utils method easy board token series level 
+linked type of references to utils typages.
+**/ 
+class MyMatrixGNUBinDlangFaps
+{
+    interface MyFaps
+    {
+        const faps(long hackers)(ref hackers){
+        auto retro(Range)(Range r)
+        if (isBidirectionalRange!(Unqual!Range)); 
+        int[5] a = [ 1, 2, 3, 4, 5 ];
+        int[5] b = [ 5, 4, 3, 2, 1 ];
+        assert(equal(retro(a[]), b[]));
+        assert(retro(a[]).source is a[]);
+        assert(retro(retro(a[])) is a[]);
+
+        }        
+    }    
+}
